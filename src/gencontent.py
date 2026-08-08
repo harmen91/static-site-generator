9fd4b99972f76723f14d6d9fff58571634576f0e
harmen91 <harmen91@gmail.com>
2026-08-08 14:03:25 +0100

    add nojekyll fix

--- src/gencontent.py
+++ src/gencontent.py
@@ -23,7 +23,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
     title = extract_title(markdown)
 
-    print(basepath)
+    print(repr(basepath))
     result = template.replace("{{ Title }}", title).replace("{{ Content }}", md_to_htmlstring).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
